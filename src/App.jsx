a8abf64630e5aa5d7a93b043ad7746a966bee8a0
braiancalpar <braian.santos@calpar.com.br>
2026-06-03 16:03:39 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import { use, useState } from "react";
+import { ChecklistsWrapper } from "./components/ChecklistsWrapper";
+import { Container } from "./components/Container";
+import { Dialog } from "./components/Dialog";
+import { FabButton } from "./components/FabButton";
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
+import { Heading } from "./components/Heading";
+import { IconPlus, IconSchool } from "./components/icons";
+import { ToDoForm } from "./components/ToDoForm";
+import TodoContext from "./components/TodoProvider/TodoContext";
+import { TodoGroup } from "./components/TodoGroup";
+
+function App() {
+  const [showDialog, setShowDialog] = useState(false);
+  const { todos, addTodo } = use(TodoContext);
+
+  const toggleDialog = () => {
+    setShowDialog(!showDialog);
+  };
+
+  const handleFormSubmit = (formData) => {
+    addTodo(formData);
+    toggleDialog();
+  };
+
+  return (
+    <main>
+      <Container>
+        <Header>
+          <Heading>
+            <IconSchool /> Plano de estudos
+          </Heading>
+        </Header>
+        <ChecklistsWrapper>
+          <TodoGroup
+            heading="Para estudar"
+            items={todos.filter((t) => !t.completed)}
+          />
+          <TodoGroup
+            heading="Concluído"
+            items={todos.filter((t) => t.completed)}
+          />
+          <Footer>
+            <Dialog isOpen={showDialog} onClose={toggleDialog}>
+              <ToDoForm onSubmit={handleFormSubmit} />
+            </Dialog>
+            <FabButton onClick={toggleDialog}>
+              <IconPlus />
+            </FabButton>
+          </Footer>
+        </ChecklistsWrapper>
+      </Container>
+    </main>
+  );
+}
+
+export default App;
